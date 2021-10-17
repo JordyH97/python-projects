77735d238d1fbda0d1cc7dfbfc6d800465d10f7f
JordyH97 <55515061+JordyH97@users.noreply.github.com>
2021-10-16 20:16:49 -0400

    Add files via upload

---
+++ tempCtoF.py
@@ -0,0 +1,5 @@
+print("Enter the temperature in Celsius")
+temperatureC = input()
+celsius = float(temperatureC)
+converF = (celsius * 1.8) + 32
+print("The temperature in Fahrenheit is:", converF)
---
+++ tempFtoC.py
@@ -0,0 +1,5 @@
+print("Enter the temperature in Fahrenheit")
+temperatureF = input()
+fahrenheit = float(temperatureF)
+converC = (fahrenheit - 32) / 1.8
+print("The temperature in Celsius is:", converC)
